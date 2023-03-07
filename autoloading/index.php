9d13fcbf6a1745e333eb8c895bd1e803dbfa4676
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-03-07 14:40:35 +0700

    Materi Autoloading

---
+++ autoloading/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'App/init.php';
+
+$produk1 = new Komik("Doraemon", "Fujiko F. Fujio", "Shogakukan", 10000, 100);
+$produk2 = new Game("Pro Evolution Soccer 2021", "Konami", "Konami", 240000, 5);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+echo $cetakProduk->cetak();
+echo '<hr>';
+new Coba;
